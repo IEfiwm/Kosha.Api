9093e34f00c085d86d2bafe059a2ca1612c408f6
Marzie Keshavarz <marziekeshavarz97@gmail.com>
2021-12-14 20:57:58 +0330

    dbf creator , stimulsoft debug

---
+++ FishHoghoghi/Business/Utilities/DBFCreator.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishHoghoghi.Business.Utilities
+{
+    public static class DBFCreator
+    {
+        public static HttpResponseMessage DataSetIntoDBF( DataSet dataSet)
+        {
+            ArrayList list = new ArrayList();
+            string fileName = "1";
+            var Path = System.Web.HttpContext.Current.Server.MapPath("~/Content/Reports/DBF/");
+            if (File.Exists(Path + fileName + ".dbf"))
+            {
+                File.Delete(Path + fileName + ".dbf");
+            }
+
+            string createSql = "create table " + fileName + " (";
+
+            foreach (DataColumn dc in dataSet.Tables[0].Columns)
+            {
+                string fieldName = dc.ColumnName;
+
+                string type = dc.DataType.ToString();
+
+                switch (type)
+                {
+                    case "System.String":
+                        type = "varchar(100)";
+                        break;
+
+                    case "System.Boolean":
+                        type = "varchar(10)";
+                        break;
+
+                    case "System.Int32":
+                        type = "int";
+                        break;
+
+                    case "System.Double":
+                        type = "Double";
+                        break;
+
+                    case "System.DateTime":
+                        type = "varchar(100)";
+                        break;
+                }
+
+                createSql = createSql + "[" + fieldName + "]" + " " + type + ",";
+
+                list.Add(fieldName);
+            }
+
+            createSql = createSql.Substring(0, createSql.Length - 1) + ")";
+
+            OleDbConnection con = new OleDbConnection(GetConnection(Path));
+
+            OleDbCommand cmd = new OleDbCommand();
+
+            cmd.Connection = con;
+
+            con.Open();
+
+            cmd.CommandText = createSql;
+
+            cmd.ExecuteNonQuery();
+
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+            {
+                string insertSql = "insert into " + fileName + " values(";
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    insertSql = insertSql + "'" + ReplaceEscape(row[list[i].ToString()].ToString()) + "',";
+                }
+
+                insertSql = insertSql.Substring(0, insertSql.Length - 1) + ")";
+
+                cmd.CommandText = insertSql;
+
+                cmd.ExecuteNonQuery();
+            }
+
+            con.Close();
+            Stream file = new MemoryStream();
+            FileStream fs = System.IO.File.OpenRead(Path + fileName + ".dbf");
+            byte[] buffer = new byte[1024];
+
+            int byteRead = 0;
+
+            do
+            {
+                byteRead = fs.Read(buffer, 0, 1024);
+
+                file.Write(buffer, 0, byteRead);
+
+            }
+            while (byteRead != 0);
+
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.StatusCode = HttpStatusCode.OK;
+            response.Content = new StreamContent(file);
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "DBF_" + Guid.NewGuid().ToString("N").Remove(6) + ".dbf"
+            };
+            fs.Close();
+            file.Close();
+            File.Delete(Path + fileName + ".dbf");
+
+            return response;
+
+        }
+
+        private static string GetConnection(string path)
+        {
+            return "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties=dBASE IV;";
+        }
+
+        public static string ReplaceEscape(string str)
+        {
+            str = str.Replace("'", "''");
+            return str;
+        }
+
+    }
+}
--- FishHoghoghi/Controllers/ReportController.cs
+++ FishHoghoghi/Controllers/ReportController.cs
@@ -1,37 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using FishHoghoghi.Business.Utilities;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Dictionary;
+using Stimulsoft.Report.Mvc;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace FishHoghoghi.Controllers
 {
-    public class ReportController : Controller
+    public class ReportController : ApiController
     {
         public ReportController()
         {
-            Stimulsoft.Base.StiLicense.Key =
-                "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHn0s4gy0Fr5YoUZ9V00Y0igCSFQzwEqYBh/N77k" +
-                "4f0fWXTHW5rqeBNLkaurJDenJ9o97TyqHs9HfvINK18Uwzsc/bG01Rq+x3H3Rf+g7AY92gvWmp7VA2Ux" +
-                "a30Q97f61siWz2dE5kdBVcCnSFzC6awE74JzDcJMj8OuxplqB1CYcpoPcOjKy1PiATlC3UsBaLEXsok1" +
-                "xxtRMQ283r282tkh8XQitsxtTczAJBxijuJNfziYhci2jResWXK51ygOOEbVAxmpflujkJ8oEVHkOA/C" +
-                "jX6bGx05pNZ6oSIu9H8deF94MyqIwcdeirCe60GbIQByQtLimfxbIZnO35X3fs/94av0ODfELqrQEpLr" +
-                "pU6FNeHttvlMc5UVrT4K+8lPbqR8Hq0PFWmFrbVIYSi7tAVFMMe2D1C59NWyLu3AkrD3No7YhLVh7LV0" +
-                "Tttr/8FrcZ8xirBPcMZCIGrRIesrHxOsZH2V8t/t0GXCnLLAWX+TNvdNXkB8cF2y9ZXf1enI064yE5dw" +
-                "Ms2fQ0yOUG/xornE";
+            //Stimulsoft.Base.StiLicense.Key =
+            //    "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHn0s4gy0Fr5YoUZ9V00Y0igCSFQzwEqYBh/N77k" +
+            //    "4f0fWXTHW5rqeBNLkaurJDenJ9o97TyqHs9HfvINK18Uwzsc/bG01Rq+x3H3Rf+g7AY92gvWmp7VA2Ux" +
+            //    "a30Q97f61siWz2dE5kdBVcCnSFzC6awE74JzDcJMj8OuxplqB1CYcpoPcOjKy1PiATlC3UsBaLEXsok1" +
+            //    "xxtRMQ283r282tkh8XQitsxtTczAJBxijuJNfziYhci2jResWXK51ygOOEbVAxmpflujkJ8oEVHkOA/C" +
+            //    "jX6bGx05pNZ6oSIu9H8deF94MyqIwcdeirCe60GbIQByQtLimfxbIZnO35X3fs/94av0ODfELqrQEpLr" +
+            //    "pU6FNeHttvlMc5UVrT4K+8lPbqR8Hq0PFWmFrbVIYSi7tAVFMMe2D1C59NWyLu3AkrD3No7YhLVh7LV0" +
+            //    "Tttr/8FrcZ8xirBPcMZCIGrRIesrHxOsZH2V8t/t0GXCnLLAWX+TNvdNXkB8cF2y9ZXf1enI064yE5dw" +
+            //    "Ms2fQ0yOUG/xornE";
+            Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHn0s4gy0Fr5YoUZ9V00Y0igCSFQzwEqYBh/N77k4f0fWXTHW5rqeBNLkaurJDenJ9o97TyqHs9HfvINK18Uwzsc/bG01Rq+x3H3Rf+g7AY92gvWmp7VA2Uxa30Q97f61siWz2dE5kdBVcCnSFzC6awE74JzDcJMj8OuxplqB1CYcpoPcOjKy1PiATlC3UsBaLEXsok1xxtRMQ283r282tkh8XQitsxtTczAJBxijuJNfziYhci2jResWXK51ygOOEbVAxmpflujkJ8oEVHkOA/CjX6bGx05pNZ6oSIu9H8deF94MyqIwcdeirCe60GbIQByQtLimfxbIZnO35X3fs/94av0ODfELqrQEpLrpU6FNeHttvlMc5UVrT4K+8lPbqR8Hq0PFWmFrbVIYSi7tAVFMMe2D1C59NWyLu3AkrD3No7YhLVh7LV0Tttr/8FrcZ8xirBPcMZCIGrRIesrHxOsZH2V8t/t0GXCnLLAWX+TNvdNXkB8cF2y9ZXf1enI064yE5dwMs2fQ0yOUG/xornE";
+
         }
         // GET: Report
-        public ActionResult InsuranceAll()
+        [HttpGet]
+        [Route("Report/InsuranceAll/{year}/{month}")]
+        public HttpResponseMessage InsuranceAll(int year, int month)
         {
-
             var report = new StiReport();
-            var path = StiNetCoreHelper.MapPath(this, "Reports/InsuranceReportAll.mrt");
+
+            var path = System.Web.HttpContext.Current.Server.MapPath("~/Content/Reports/InsuranceReportAll.mrt");
+
             report.Load(path);
 
             var dbMS_SQL = (StiSqlDatabase)report.Dictionary.Databases["MS SQL"];
-            dbMS_SQL.ConnectionString = @"Data Source=2.144.243.200;Initial Catalog=Kosha_MTJA;User Id=ngra;Password=ngra@123456789;MultipleActiveResultSets=True;";
+            dbMS_SQL.ConnectionString = ConfigurationManager.ConnectionStrings["Sg3ConnectionString"].ConnectionString;
 
-            return StiNetCoreReportResponse.ResponseAsPdf(report);
+            report.Dictionary.Variables["Month"].ValueObject = month;
+
+            report.Dictionary.Variables["Year"].ValueObject = year;
+
+            report.ReportName = Guid.NewGuid().ToString("N").Remove(8);
+
+            var response = StiMvcReportResponse.ResponseAsPdf(report).ToHttpResponseMessage();
+
+            return response;
         }
+
+
+
+        [HttpGet]
+        [Route("Report/DBFAll/{year}/{month}")]
+        public HttpResponseMessage DBFAll(int year, int month)
+        {
+            var ConnectionString = ConfigurationManager.ConnectionStrings["Sg3ConnectionString"].ConnectionString;
+            string sCMD_All = "dbo.ProcKosha_DSW";
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+
+            using (SqlConnection myConn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand myCommand = new SqlCommand(sCMD_All, myConn))
+                {
+                    myConn.Open();
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.Parameters.AddWithValue("@year", SqlDbType.NVarChar).Value = year.ToString();
+                    myCommand.Parameters.AddWithValue("@month", SqlDbType.NVarChar).Value = month.ToString();
+                    da = new SqlDataAdapter(sCMD_All, myConn);
+                    da.SelectCommand = myCommand;
+                    da.Fill(ds, "DBF");
+                    myConn.Close();
+                }
+            }
+            var response =  DBFCreator.DataSetIntoDBF( ds);
+            return response;
+        }
+
     }
 }
