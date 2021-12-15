51ff70b1a05caada6391136e597c07585a74bb55
Marzie Keshavarz <marziekeshavarz97@gmail.com>
2021-12-15 18:46:35 +0330

    nothing

--- FishHoghoghi/Controllers/ReportController.cs
+++ FishHoghoghi/Controllers/ReportController.cs
@@ -17,19 +17,9 @@ namespace FishHoghoghi.Controllers
     {
         public ReportController()
         {
-            //Stimulsoft.Base.StiLicense.Key =
-            //    "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHn0s4gy0Fr5YoUZ9V00Y0igCSFQzwEqYBh/N77k" +
-            //    "4f0fWXTHW5rqeBNLkaurJDenJ9o97TyqHs9HfvINK18Uwzsc/bG01Rq+x3H3Rf+g7AY92gvWmp7VA2Ux" +
-            //    "a30Q97f61siWz2dE5kdBVcCnSFzC6awE74JzDcJMj8OuxplqB1CYcpoPcOjKy1PiATlC3UsBaLEXsok1" +
-            //    "xxtRMQ283r282tkh8XQitsxtTczAJBxijuJNfziYhci2jResWXK51ygOOEbVAxmpflujkJ8oEVHkOA/C" +
-            //    "jX6bGx05pNZ6oSIu9H8deF94MyqIwcdeirCe60GbIQByQtLimfxbIZnO35X3fs/94av0ODfELqrQEpLr" +
-            //    "pU6FNeHttvlMc5UVrT4K+8lPbqR8Hq0PFWmFrbVIYSi7tAVFMMe2D1C59NWyLu3AkrD3No7YhLVh7LV0" +
-            //    "Tttr/8FrcZ8xirBPcMZCIGrRIesrHxOsZH2V8t/t0GXCnLLAWX+TNvdNXkB8cF2y9ZXf1enI064yE5dw" +
-            //    "Ms2fQ0yOUG/xornE";
             Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHn0s4gy0Fr5YoUZ9V00Y0igCSFQzwEqYBh/N77k4f0fWXTHW5rqeBNLkaurJDenJ9o97TyqHs9HfvINK18Uwzsc/bG01Rq+x3H3Rf+g7AY92gvWmp7VA2Uxa30Q97f61siWz2dE5kdBVcCnSFzC6awE74JzDcJMj8OuxplqB1CYcpoPcOjKy1PiATlC3UsBaLEXsok1xxtRMQ283r282tkh8XQitsxtTczAJBxijuJNfziYhci2jResWXK51ygOOEbVAxmpflujkJ8oEVHkOA/CjX6bGx05pNZ6oSIu9H8deF94MyqIwcdeirCe60GbIQByQtLimfxbIZnO35X3fs/94av0ODfELqrQEpLrpU6FNeHttvlMc5UVrT4K+8lPbqR8Hq0PFWmFrbVIYSi7tAVFMMe2D1C59NWyLu3AkrD3No7YhLVh7LV0Tttr/8FrcZ8xirBPcMZCIGrRIesrHxOsZH2V8t/t0GXCnLLAWX+TNvdNXkB8cF2y9ZXf1enI064yE5dwMs2fQ0yOUG/xornE";
 
         }
-        // GET: Report
         [HttpGet]
         [Route("Report/InsuranceAll/{year}/{month}")]
         public HttpResponseMessage InsuranceAll(int year, int month)
@@ -54,7 +44,6 @@ namespace FishHoghoghi.Controllers
             return response;
         }
 
-
         [HttpGet]
         [Route("Report/InsuranceSummary/{year}/{month}")]
         public HttpResponseMessage InsuranceSummary(int year, int month)
@@ -78,8 +67,6 @@ namespace FishHoghoghi.Controllers
 
             return response;
         }
-
-
 
         [HttpGet]
         [Route("Report/DBFSummary/{year}/{month}")]
