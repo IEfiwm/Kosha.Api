774909a76430fefccb3f7c9b9f17b006098807d9
Marzie Keshavarz <marziekeshavarz97@gmail.com>
2022-03-18 13:15:01 +0330

    debug

--- FishHoghoghi/Controllers/CalculationController.cs
+++ FishHoghoghi/Controllers/CalculationController.cs
@@ -1,12 +1,16 @@
 ﻿using FishHoghoghi.Attribute;
 using FishHoghoghi.Business.Dal;
 using FishHoghoghi.Structure;
+using FishHoghoghi.Structure.Models;
 using Kosha.Core.Common.Helper;
 using Kosha.Core.Common.Model;
 using Kosha.Core.Contract.AuthenticationCode;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 using Common = FishHoghoghi.Utilities.Utility;
 
@@ -25,42 +29,73 @@ namespace FishHoghoghi.Controllers
         [HttpGet]
         public HttpResponseMessage GenerateSalary(long projectId, string year, string month)
         {
-            var rules = Project.GetRules(projectId);
 
-            string command = ",";
+            var fieldFormules = Project.GetFieldRules(projectId);
+            List<FieldRuleModel> model = new List<FieldRuleModel>();
+            string command = "";
+            var insertQuery = " insert into Data.TbImported (year,month,";
 
-            for (int i = 0, count = rules.Rows.Count; i < count--; i++, command += ",")
+            for (int i = 0, count = fieldFormules.Rows.Count; i < count--; i++)
             {
-                command += $@"{rules.Rows[i]["Rule"].ToString().Replace("&", "")}  AS  [{rules.Rows[i]["Name"]}]";
+                model.Add(new FieldRuleModel
+                {
+                    Name = fieldFormules.Rows[i]["Name"]?.ToString(),
+                    Alias = fieldFormules.Rows[i]["Alias"]?.ToString(),
+                    Formule = fieldFormules.Rows[i]["Formule"]?.ToString()
+                });
             }
+
+
+            foreach (var item in model)
+            {
+                item.Formule = cal(item.Formule, model);
+            }
+
+            foreach (var item in model)
+            {
+                command += $@"{item.Formule.Replace("&", "")}  AS  [{item.Name}],";
+                insertQuery += item.Name + ",";
+            }
+
 
             command = command.Remove(command.Length - 1);
+            insertQuery = insertQuery.Remove(insertQuery.Length - 1);
+            insertQuery += ") ";
 
-            var fields = Project.GetFields();
 
-            string fieldsCommand = "INSERT  INTO    [SM_Test].[Data].[TbField]  (";
+            insertQuery += $@"SELECT	 {year},{month},{command} FROM	[dbo].[Kosha_Attendances] 
+                    WHERE   [ProjectId] =   {projectId}    AND [سال]   = '{year}'    AND [ماه] =   '{month}'";
 
-            for (int i = 0, count = fields.Rows.Count; i < count--; i++, fieldsCommand += ",")
-            {
-                fieldsCommand += $@"{fields.Rows[i]["Name"].ToString()}";
-            }
 
-            fieldsCommand = fieldsCommand.Remove(fieldsCommand.Length - 1);
+            //delete from imported
+            //insert 
 
-            fieldsCommand += ")";
-
-            var attendances = Project.GetAttendances(projectId, year, month, command);
-
-            for (int i = 0, count = attendances.Rows.Count; i < count--; i++)
-            {
-                var test = attendances.Rows[i][fields.Rows[i]["Alias"].ToString()];
-            }
-
-            //if (rules.Table.Rows.Count == 0 || attendances.Table.Rows.Count == 0)
-            //{
-            //    return null;
-            //}
             return null;
+        }
+
+        private string cal(string formule, List<FieldRuleModel> model)
+        {
+            Regex regex = new Regex(@"\[(?<Col>([^\[|\]]*))+\]$");
+            var groups = formule.Split('&'); /// [staticField] & * & [dynamicField]
+            if (groups.Length > 0)
+            {
+                foreach (var word in groups)
+                {
+                    if (word.Contains("["))// [dynamicField] 
+                    {
+                        Match getCol = regex.Match(word.Trim());
+                        string col = getCol.Value?.Replace("[", "").Replace("]", "").Trim();//dynamicField
+
+                        if (model.Any(x => x.Alias == col))
+                        {
+                            var childModel = model.FirstOrDefault(x => x.Alias == col);
+                            formule = formule.Replace("[" + col + "]", "(" + childModel.Formule + ")");
+                            formule = cal(formule, model);
+                        }
+                    }
+                }
+            }
+            return formule;
         }
     }
 }
---
+++ FishHoghoghi/Structure/Models/FieldRuleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FishHoghoghi.Structure.Models
+{
+    public class FieldRuleModel
+    {
+        public string Name { get; set; }
+
+        public string Alias { get; set; }
+
+        public string Formule { get; set; }
+    }
+}
